56ecd691cf401f0f696d82de7aebee16cccaf744
Jogeir Liavåg <jogeir@tutanota.com>
2026-02-11 13:48:59 +0100

    add subtract, multiply and divide method

--- src/main/java/org/ntnu/serverclient/ArithmeticOperations.java
+++ src/main/java/org/ntnu/serverclient/ArithmeticOperations.java
@@ -6,4 +6,17 @@ public class ArithmeticOperations {
     return a + b;
   }
 
+  public static double sub(double a, double b) {
+    return a - b;
+  }
+
+  public static double mul(double a, double b) {
+    return a * b;
+  }
+
+  public static double div(double a, double b) {
+    return a / b;
+  }
+
+
 }
